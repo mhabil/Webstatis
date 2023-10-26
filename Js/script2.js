a7019237c2a9e2a852fa2154c8121491c765d3ee
mhabil <mhabil.46@gmail.com>
2023-10-27 06:22:33 +0700

    new JS

--- Js/script2.js
+++ Js/script2.js
@@ -1,6 +1,49 @@
-alert("HELLO HABIL");
+// alert("selamat datang..");
+// var lagi = true;
 
-h2("INI \n BERITA");
-"HABIL TAMVAN".length;
-var x = 10;
-Boolean(10 < 20);
+// while (lagi === true) {
+//   var nama = prompt("masukkan nama");
+//   alert("Halo " + nama);
+
+//   lagi = confirm("coba lagi?");
+// }
+
+// alert("terimakasih..");
+
+// toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburgermenu diklik
+document.querySelector("#hamburger-menu").onclick = (e) => {
+  e.preventDefault();
+  navbarNav.classList.toggle("active");
+};
+
+// klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+{
+  /* <button
+  type="button"
+  onclick="document.getElementById('about).style.display='block'"
+>
+  Click Me!
+</button>; */
+}
+
+// function toggleParagraph() {
+//   const sejarahParagraph = document.getElementById("sejarah-paragraph");
+//   if (
+//     sejarahParagraph.style.display === "block" ||
+//     sejarahParagraph.style.display === ""
+//   ) {
+//     sejarahParagraph.style.display = "none"; // Sembunyikan paragraf jika sedang tampil
+//   } else {
+//     sejarahParagraph.style.display = "block"; // Tampilkan paragraf jika sedang tersembunyi
+//   }
+// }
